4213826949337bd753e28d6e9deaa5c871295afa
amirshahidi3500 <amirshahidi.4335@gmail.com>
2026-09-25 22:47:58 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("com.google.devtools.ksp")
+    id("com.android.application") version "8.13.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.0" apply false
+    id("com.google.devtools.ksp") version "2.3.4" apply false
 }
 
 android {
